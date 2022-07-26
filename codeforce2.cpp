b0bcf46a56d466bf3075a7266f1d7dca1a9d7966
ttahasina <109543315+ttahasina@users.noreply.github.com>
2022-07-26 11:03:43 -0700

    Add files via upload

---
+++ codeforce2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        if(s.size()>10){
+            cout<<s[0]<<s.size()-2<<s[s.size()-1]<<'\n';}
+        else
+            cout<<s<<'\n';
+
+    }
+    return 0;
+}
---
+++ codeforce3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,p,v,t,solve=0;
+    cin>> n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>p>>v>>t;
+        if((p==1 && v==1)||(t==1 && p==1)||(v==1&&t==1))
+            solve++;
+    }
+    cout<<solve<<'\n';
+    return 0;
+}
+
+
