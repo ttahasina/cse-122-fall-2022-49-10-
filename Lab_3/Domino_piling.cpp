9872cf4b3740fb60d8c2caaf6864a506f09f2174
Tahasina Nur <109543315+ttahasina@users.noreply.github.com>
2022-10-17 12:30:35 -0700

    Create Domino_piling.cpp

---
+++ Lab_3/Domino_piling.cpp
@@ -0,0 +1,11 @@
+//codeforces problem no.50A
+//codeforces problem name:Dominopiling
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    cout<<x*y/2<<'\n';
+    return 0;
+}
