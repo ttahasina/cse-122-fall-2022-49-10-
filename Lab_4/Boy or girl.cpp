8fd379c4e0874e0c778347137e90262b6086927f
Tahasina Nur <109543315+ttahasina@users.noreply.github.com>
2022-10-17 12:50:51 -0700

    Create Boy or girl.cpp

---
+++ Lab_4/Boy or girl.cpp
@@ -0,0 +1,23 @@
+//codeforces problem no: 236A
+//codeforces problem name: Boy or girl
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	sort(&s[0],&s[s.length()]);
+	int count=0;
+	for(int i=0;i<s.length();i++)
+	{
+		if(s[i]==s[i-1])
+		{
+			continue;
+		}
+		count++;
+	}
+	if(count%2)
+		cout<<"IGNORE HIM!\n";
+	else
+		cout<<"CHAT WITH HER!\n";
+}
