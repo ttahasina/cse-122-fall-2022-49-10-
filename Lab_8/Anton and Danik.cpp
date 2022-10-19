daccecf8e45a5214a9e2f697a5df48ee20038cf2
Tahasina Nur <109543315+ttahasina@users.noreply.github.com>
2022-10-19 08:56:16 +0600

    Create Anton and Danik.cpp

---
+++ Lab_8/Anton and Danik.cpp
@@ -0,0 +1,36 @@
+//codeforces problem no: 734A
+//codeforces problem name: Anton and Danik
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a,count1 = 0,count2 = 0;
+    cin >> a;
+    string s;
+    cin >> s;
+    for(int i = 0; i < a; i++)
+    {
+        if(s[i] == 'A')
+        {
+            count1++;
+        }
+        else if(s[i] == 'D')
+        {
+            count2++;
+        }
+    }
+    if(count1 > count2)
+    {
+        cout << "Anton";
+    }
+    else if(count1 < count2)
+    {
+        cout << "Danik";
+    }
+    else if(count1 == count2)
+    {
+        cout << "Friendship";
+    }
+}
+Footer
